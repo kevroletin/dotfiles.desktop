5d160203bb16f0c02eeea3bb925abee56a854d61
Vasiliy Kevroletin <kevroletin@gmail.com>
2015-12-26 17:11:10 +1000

    Format code to get rid of deeply nested structures
    Split keys configuration into smaller functions.
    Add types for each top-level definition.

--- xmonad.hs
+++ xmonad.hs
@@ -1,48 +1,62 @@
-import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
-import XMonad.Util.Scratchpad
-import XMonad.Actions.CycleWS
-import XMonad.Actions.CycleRecentWS
-import XMonad.Hooks.ManageHelpers
-import XMonad.Layout.NoBorders
-import qualified XMonad.StackSet as W
-import qualified Data.Map as M
-import Control.Monad
+import           Control.Monad
+import qualified Data.Map                     as M
+import           Data.Monoid                  ()
+import           System.IO
+import           XMonad
+import           XMonad.Actions.CycleRecentWS
+import           XMonad.Actions.CycleWS
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Hooks.ManageHelpers
+import           XMonad.Layout.NoBorders
+import qualified XMonad.StackSet              as W
+import           XMonad.Util.EZConfig         ()
+import           XMonad.Util.Run              (spawnPipe)
+import           XMonad.Util.Scratchpad
 
--- Define keys to add
-keysToAdd x =
-    [
-        -- Cycle workspaces
-           ((modMask x                , xK_Left), prevWS)
-        ,  ((modMask x                , xK_Right), nextWS)
-        ,  (((modMask x .|. shiftMask), xK_Left), shiftToPrev)
-        ,  (((modMask x .|. shiftMask), xK_Right), shiftToNext)
-        -- Cycle recent workspaces. Mod + Tab to activate. Arrows to switch. Esc - exit.
-        , ((modMask x, xK_Tab), cycleRecentWS [xK_Escape] xK_Left xK_Right)
-           
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((modMask x, xK_quoteleft), scratchpadSpawnActionTerminal "xterm")
-        , ((0, xK_Print), spawn "scrot")
-        , (((modMask x .|. shiftMask), xK_m), mapM_ spawn ["skype", "firefox", "thunderbird"])
-        , (((modMask x), xK_F1), spawn "xprop >> /tmp/xprop.log")
-    ]
 
--- Define keys to remove
-keysToRemove x =
-    [
-        -- Unused gmrun binding
-          (modMask x .|. shiftMask, xK_p)
-        , (modMask x, xK_Tab)
-    ]
+type KeyCombination = (KeyMask, KeySym)
+type KeyBinding = (KeyCombination, X ())
 
+-- Custom key bindings
+keysToAdd :: XConfig l -> [KeyBinding]
+keysToAdd x = [
+   -- Move view to right or left workspace
+     ((modMask x                , xK_Left), prevWS)
+  ,  ((modMask x                , xK_Right), nextWS)
+
+  -- Move focused program to right or left workspace
+  ,  (((modMask x .|. shiftMask), xK_Left), shiftToPrev)
+  ,  (((modMask x .|. shiftMask), xK_Right), shiftToNext)
+
+  -- Mod + Tab enters "cycle through history" mode. Arrows to switch. Esc - exit.
+  , ((modMask x, xK_Tab), cycleRecentWS [xK_Escape] xK_Left xK_Right)
+
+  -- Handle print screen using scrot utility. Resulting pictures are in in ~/Pictures
+  , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+  , ((modMask x, xK_quoteleft), scratchpadSpawnActionTerminal "xterm")
+  , ((0, xK_Print), spawn "scrot")
+  , (((modMask x .|. shiftMask), xK_m), mapM_ spawn ["skype", "firefox", "thunderbird"])
+  , (((modMask x), xK_F1), spawn "xprop >> /tmp/xprop.log")
+  ]
+
+-- Unused default key bindings
+keysToRemove :: XConfig l -> [KeyCombination]
+keysToRemove x = [
+  -- Xmobar is used as programs launcher
+    (modMask x .|. shiftMask, xK_p)
+
+  -- This one used for history cycle
+  , (modMask x, xK_Tab)
+  ]
+
+-- Modify default key bindings scheme
+myKeys :: XConfig Layout -> M.Map (KeyCombination) (X ())
 myKeys x = M.union (strippedKeys x) (M.fromList (keysToAdd x))
   where
-    strippedKeys x = foldr M.delete (keys defaultConfig x) (keysToRemove x)
+    strippedKeys t = foldr M.delete (keys defaultConfig t) (keysToRemove t)
 
+main :: IO ()
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar /home/behemoth/.xmonad/.xmobarrc"
     xmonad $ defaultConfig
@@ -60,7 +74,7 @@ main = do
         , startupHook = do screenWorkspace 1 >>= flip whenJust (windows . W.view)
                            windows $ W.greedyView "chat"
                            screenWorkspace 0 >>= flip whenJust (windows . W.view)
---                           windows $ W.greedyView "web"
+                           windows $ W.greedyView "web"
                            return ()
         , workspaces = myWorkspaces
         }
@@ -68,18 +82,20 @@ main = do
       myLayoutHook = avoidStruts  $  layoutHook defaultConfig
       noScratchPad ws = if ws == "NSP" then "" else ws
 
+myWorkspaces :: [String]
 myWorkspaces = ["web","work","3","4","5","6","7","mail","chat","temp"]
 
+myManageHook :: ManageHook
 myManageHook = composeAll . concat $
     [
         [ className =? b --> viewShift "web"      | b <- myClassWebShifts  ]
       , [ resource  =? c --> doF (W.shift "mail") | c <- myClassMailShifts ]
       , [ resource  =? c --> doF (W.shift "chat") | c <- myClassChatShifts ]
       , [ className =? i --> doFloat | i <- myClassFloats ]
--- Allows focusing other monitors without killing the fullscreen
+      -- Allows focusing other monitors without killing the fullscreen
       , [ isFullscreen --> (doF W.focusDown <+> doFullFloat) ]
--- Single monitor setups, or if the previous hook doesn't work
---      , [ isFullscreen --> doFullFloat ]
+      -- Single monitor setups, or if the previous hook doesn't work
+      -- , [ isFullscreen --> doFullFloat ]
     ]
     where
         viewShift = doF . liftM2 (.) W.greedyView W.shift
