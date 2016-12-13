3a63ada5dc53aa77b55b5e8e81f59353774b5ddc
Vasiliy Kevroletin <kevroletin@gmail.com>
2016-12-13 17:17:59 +1000

    Move mr config into this repo

--- .dotfiles_bootstrap.d/desktop.sh
+++ .dotfiles_bootstrap.d/desktop.sh
@@ -1,20 +1,21 @@
-#!/bin/sh
-
+#!exec sh
 
 PACKAGES="xmonad libghc-xmonad-contrib-dev xmobar rxvt-unicode trayer xclip dmenu"
-echo "=== Installing $PACKAGES ==="
-sudo apt-get install $PACKAGES
 
-echo "=== Configuring desktop session ==="
+cat <<EOF >> $RESULT_SCRIPT_FILE
+"=== Configuring desktop session ==="
 SESSION_FILE=/usr/share/xsessions/xmonad.desktop
 sudo sed -i "s/Exec=xmonad-session/Exec=\/home\/$USER\/.xmonad\/.xsessionrc/g" $SESSION_FILE
 sudo sed -i "s/Exec=xmonad/Exec=\/home\/$USER\/.xmonad\/.xsessionrc/g" $SESSION_FILE
+EOF
 
 while true; do
     read -p "Do you wish to install gui version of emacs? " yn
     case $yn in
-        [Yy]* ) sudo apt-get install emacs; break;;
+        [Yy]* ) PACKAGES="$PACKAGES emacs"; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+echo $PACKAGES > $RESULT_PACKAGES_FILE
