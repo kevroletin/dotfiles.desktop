f598a705abb44879efa65de898ce060aa547cc02
Vasiliy Kevroletin <kevroletin@gmail.com>
2022-06-08 15:40:40 +0800

    Update desktop.sh

--- .dotfiles_bootstrap.d/desktop.sh
+++ .dotfiles_bootstrap.d/desktop.sh
@@ -1,6 +1,6 @@
 #!exec sh
 
-PACKAGES="xmonad libghc-xmonad-contrib-dev xmobar rxvt-unicode trayer xclip dmenu xloadimage redshift"
+PACKAGES="xmonad libghc-xmonad-contrib-dev xmobar rxvt-unicode trayer xclip dmenu xloadimage redshift feh"
 
 cat <<EOF >> $RESULT_SCRIPT_FILE
 "=== Configuring desktop session ==="
